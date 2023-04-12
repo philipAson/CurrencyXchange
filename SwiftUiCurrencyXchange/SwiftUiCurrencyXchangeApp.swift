b84b4cd6c7bd5b96a6e588248fbe562a9b38d64a
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-04-12 15:46:55 +0200

    Initial Commit

---
+++ SwiftUiCurrencyXchange/SwiftUiCurrencyXchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiCurrencyXchangeApp.swift
+//  SwiftUiCurrencyXchange
+//
+//  Created by Philip Andersson on 2023-04-12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiCurrencyXchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
